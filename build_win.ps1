70995d8612f39acee4277227573ad3612505737b
K. Joeri van der Velde <joeriv@gmail.com>
2026-07-09 11:59:55 +0200

    Build cmd for Windows PowerShell

---
+++ build_win.ps1
@@ -0,0 +1,17 @@
+$args = @(
+    "--noconfirm"
+    "--clean"
+    "--onedir"
+    "--name", "AGInterpet"
+    "--collect-all", "streamlit"
+    "--collect-all", "alphagenome"
+    "--collect-all", "plotly"
+    "--collect-all", "pandas"
+    "--add-data", "main.py:."
+    "--add-data", "plot_functions.py:."
+    "--add-data", "resources:resources"
+    "--add-data", ".streamlit:.streamlit"
+    "--add-data", "static:static"
+    "app_launcher.py"
+)
+py -m PyInstaller @args
